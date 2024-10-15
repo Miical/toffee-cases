03ad61373da08d40993b2d0c70a3d40458ca440b
Miical <879754743@qq.com>
2024-10-15 12:39:40 +0800

    Adder adder mlvp case

---
+++ adder/env.py
@@ -0,0 +1,35 @@
+from mlvp import *
+from mlvp.triggers import *
+from mlvp.agent import *
+from mlvp.model import *
+
+class AdderBundle(Bundle):
+	a, b, cin, sum, cout = Signals(5)
+
+class AdderAgent(Agent):
+    def __init__(self, bundle):
+        super().__init__(bundle.step)
+        self.bundle = bundle
+
+    @driver_method()
+    async def exec_add(self, a, b, cin):
+        self.bundle.a.value = a
+        self.bundle.b.value = b
+        self.bundle.cin.value = cin
+        await self.bundle.step()
+        return self.bundle.sum.value, self.bundle.cout.value
+
+class AdderModel(Model):
+    @driver_hook(agent_name="add_agent")
+    def exec_add(self, a, b, cin):
+        result = a + b + cin
+        sum = result & ((1 << 64) - 1)
+        cout = result >> 64
+        return sum, cout
+
+class AdderEnv(Env):
+    def __init__(self, adder_bundle):
+        super().__init__()
+        self.add_agent = AdderAgent(adder_bundle)
+
+        self.attach(AdderModel())
---
+++ adder/test_adder.py
@@ -0,0 +1,33 @@
+import random
+import pytest
+from mlvp import *
+from UT_Adder import DUTAdder
+from env import AdderEnv, AdderBundle
+
+@pytest.mark.mlvp_async
+async def test_random(mlvp_request):
+    env = mlvp_request()
+
+    for _ in range(1000):
+        a = random.randint(0, 2**64-1)
+        b = random.randint(0, 2**64-1)
+        cin = random.randint(0, 1)
+        await env.add_agent.exec_add(a, b, cin)
+
+@pytest.mark.mlvp_async
+async def test_boundary(mlvp_request):
+    env = mlvp_request()
+
+    for cin in [0, 1]:
+        for a in [0, 2**64-1]:
+            for b in [0, 2**64-1]:
+                await env.add_agent.exec_add(a, b, cin)
+
+@pytest.fixture()
+def mlvp_request(mlvp_pre_request: PreRequest):
+    dut = mlvp_pre_request.create_dut(DUTAdder)
+
+    def start_code():
+        start_clock(dut)
+        return AdderEnv(AdderBundle.from_prefix("io_").bind(dut))
+    return start_code
