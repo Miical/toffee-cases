a0aef10db3d6d9d66d9a380ecb37d697a085269e
Miical <879754743@qq.com>
2024-10-15 16:38:31 +0800

    Add adder scoreboard

--- adder-uvm/src/env/adder_env.sv
+++ adder-uvm/src/env/adder_env.sv
@@ -2,26 +2,25 @@
 `define ADDER_ENV__SV
 
 `include "adder_ports/adder_agent/adder_agent.sv"
-// `include "src/env/adder_scoreboard.sv"
+`include "src/env/adder_scoreboard.sv"
 
 class adder_env extends uvm_env;
     `uvm_component_utils(adder_env)
 
     adder_agent adder_ag;
-    // adder_scoreboard scoreboard;
+    adder_scoreboard scoreboard;
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
         adder_ag = adder_agent::type_id::create("adder_agent", this);
-        // scoreboard = adder_scoreboard::type_id::create("scoreboard", this);
+        scoreboard = adder_scoreboard::type_id::create("scoreboard", this);
     endfunction
 
     virtual function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
 
-        // adder_ag.req_ap.connect(scoreboard.req_ap);
-        // `uvm_info(get_type_name(), "connected", UVM_LOW)
+        adder_ag.ap.connect(scoreboard.ap);
     endfunction
 
     function new(string name, uvm_component parent);
--- adder-uvm/src/env/adder_scoreboard.sv
+++ adder-uvm/src/env/adder_scoreboard.sv
@@ -1,65 +1,24 @@
-`ifndef CACHE_SCOREBOARD__SV
-`define CACHE_SCOREBOARD__SV
+`ifndef ADDER_SCOREBOARD__SV
+`define ADDER_SCOREBOARD__SV
 
-`include "simplebus/simplebus_item.sv"
+`include "adder_ports/adder_transaction.sv"
 
-`uvm_analysis_imp_decl(_req)
-`uvm_analysis_imp_decl(_resp)
-class cache_scoreboard extends uvm_scoreboard;
-    uvm_analysis_imp_req #(simplebus_item, cache_scoreboard) req_ap;
-    uvm_analysis_imp_resp #(simplebus_item, cache_scoreboard) resp_ap;
-
-    `uvm_component_utils(cache_scoreboard)
-
-    typedef int unsigned int_map_t[int];
-    int_map_t data;
-    simplebus_item tr;
+class adder_scoreboard extends uvm_scoreboard;
+    uvm_analysis_imp #(adder_transaction, adder_scoreboard) ap;
+    `uvm_component_utils(adder_scoreboard)
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
-        tr = new("tr");
-        req_ap = new("req_ap", this);
-        resp_ap = new("resp_ap", this);
+        ap = new("ap", this);
     endfunction
 
-    virtual function write_req(simplebus_item trans);
-        int addr, wmask, wdata;
-
-        if (trans.req_cmd == READ_CMD) begin
-            addr = trans.req_addr & 32'hfffffff8;
-            tr.tr_type = simplebus_item::RESP;
-            tr.resp_cmd = READ_LAST_CMD;
-            tr.resp_user = trans.req_user;
-            for (int i = 0; i < 8; i++) begin
-                tr.resp_rdata[i] = 0;
-            end
-
-            if (data.exists(addr) == 1) begin
-                tr.resp_rdata[0] = data[addr];
-            end
-        end
-
-        else if (trans.req_cmd == WRITE_CMD) begin
-            addr = trans.req_addr & 32'hfffffff8;
-            wdata = trans.req_wdata;
-            wmask = replicate_bits(trans.req_wmask, 8, 8);
-            if (data.exists(addr) == 0) begin
-                data[addr] = 0;
-            end
-            data[addr] = (data[addr] & (~wmask)) | (wdata & wmask);
-
-            tr.tr_type = simplebus_item::RESP;
-            tr.resp_cmd = WRITE_RESP_CMD;
-            tr.resp_user = trans.req_user;
-            for (int i = 0; i < 8; i++) begin
-                tr.resp_rdata[i] = 0;
-            end
-        end
-    endfunction
-
-    virtual function write_resp(simplebus_item trans);
-        if (!trans.compare(tr)) begin
-            `uvm_error("SCOREBOARD", $sformatf("Response transaction mismatch: expected %s, got %s", tr.sprint(), trans.sprint()));
+    virtual function write(adder_transaction trans);
+        bit [64:0] expected = {1'b0, trans.a} + {1'b0, trans.b} + trans.cin;
+        if (trans.sum !== expected[63:0] || trans.cout !== expected[64]) begin
+            `uvm_error("ADDER_SCOREBOARD", $sformatf("Mismatch! Expected sum: %0h, cout: %0b, Got sum: %0h, cout: %0b",
+                        expected[63:0], expected[64], trans.sum, trans.cout))
+        end else begin
+            `uvm_info("ADDER_SCOREBOARD", "Result matches", UVM_LOW)
         end
     endfunction
 endclass
